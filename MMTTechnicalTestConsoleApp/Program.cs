1577917652283d52d060951d6730f9310af23e6e
Darren Nicholas <dnicholas@live.co.uk>
2021-01-09 16:01:49 +0000

    Getting ready to create the console App

---
+++ MMTTechnicalTestConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MMTTechnicalTestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
